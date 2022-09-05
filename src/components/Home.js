aaff7dd04d84b0279ead5d726512c930969786c0
Sarah Mukuti <cesarwrites24@gmail.com>
2022-09-05 14:37:35 +0300

    initialize react app

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home(){
+    return(
+        <div className="home-div">
+            <h1>Prodev Learning</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ src/components/Lesson.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Lesson(){
+    return(
+        <div className='lesson-div'>
+            <h1>Available Lessons</h1>
+        </div>
+    )
+}
+
+export default Lesson;
