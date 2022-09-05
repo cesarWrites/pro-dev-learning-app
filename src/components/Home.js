31ebb74dc82aa954477a5a483f1da046657de084
Sarah Mukuti <cesarwrites24@gmail.com>
2022-09-05 15:20:04 +0300

    create components

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,12 @@ import React from "react";
 function Home(){
     return(
         <div className="home-div">
+            <header>
             <h1>Prodev Learning</h1>
+            </header>
+            {courses.map((course) => (
+                 <CourseSummary course={course} key={course.id} />
+            ))}
         </div>
     )
 }
