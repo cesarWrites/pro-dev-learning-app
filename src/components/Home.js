b900cbd35cf237e0417501c9cc0d29523091f642
Sarah Mukuti <cesarwrites24@gmail.com>
2022-09-07 03:10:34 +0300

    add new form

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import CourseDetails from "./CourseDetail";
 import NavBar from "./NavBar";
 import Footer from "./footer";
+import NewCourse from "./NewCourse";
 
 function Home(){
+    let [show, setForm] = useState(false);
     const background_url = "https://codetoinvent.com/wp-content/uploads/2020/12/programming-courses-back.jpg"
+
+    function handleClick(){
+        setForm(true);
+      }
     return(
         <div className="home-div">
             <header>
             <h1>Prodev Learning</h1>
             <NavBar/>
-            <img src={background_url}/>
             </header>
+            <div>
+                <article>
+                    <p>Welcome to the Prodev learning website.</p>
+                    <p>Access courses across various programming disciplines from reputable authors.</p>
+                </article>
+            </div>
+            <div>
+            <img src={background_url}/>
+            </div>
+            <button onClick={handleClick}>Create new tip</button>
+        {show ? <NewCourse /> : null}
             <Footer/>
         </div>
     )
---
+++ src/components/NewCourse.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import axios from 'axios';
+function NewCourse(){
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    const [rating, setRating] = useState("")
+
+    const handleSubmit =(e) => {
+        e.preventDefault();
+      
+      axios.post('http://localhost:9292/courses', {
+      title:title,
+      description:description,
+      rating: rating
+    })
+    .then((response) => {
+      console.log(response)
+    }, (error) => {
+      console.log(error);
+    })
+      };
+    return(
+        <div>
+    <form className="new-tip-form" onSubmit={handleSubmit}>
+      <input placeholder="UserName" type="text" 
+      value={title} 
+      onChange={(e) => setTitle(e.target.value)}/>
+      <textarea placeholder="Write your tech tip here..." rows={10} type="text" value={description} 
+      onChange={(e) => setDescription(e.target.value)}/>
+       <input placeholder="UserName" type="text" 
+      value={rating} 
+      onChange={(e) => setRating(e.target.value)}/>
+      <input type="submit" value="Share the tech tip" className="btn-inpt"/>
+    </form>
+        </div>
+    )
+}
+
+export default NewCourse;
